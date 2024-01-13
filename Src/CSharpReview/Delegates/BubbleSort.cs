082db79f1264ae13aa0598c11112f277cca496fa
Hasanur Rashid <hasan6221@gmail.com>
2024-01-14 01:39:09 +0600

    Class 4 Commit 1

---
+++ Src/CSharpReview/Delegates/BubbleSort.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    public class BubbleSort<T>
+    {
+       // public delegate bool Compare(T a, T b);
+      //  public T[] Sort(T[] items, Compare compare) 
+        public T[] Sort(T[] items, Func <T,T,bool> compare) 
+        {
+            bool flag = true;
+            int itemNumber = items.Length;
+            T temp;
+           for (int i = 0; i< itemNumber && flag; i++)
+           {
+                 flag = false;
+                for(int j=0; j < items.Length - 1 - i; j++)
+                {
+                    
+                    if (compare(items[j] , items[j+1]))
+                    {
+                        temp = items[j];
+                        items[j] = items[j+1];
+                        items[j+1] = temp;
+                        flag = true;
+                    }
+                }
+           }
+            return items;
+        }
+    }
+}
---
+++ Src/CSharpReview/Delegates/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Delegates;
+
+BubbleSort<int> bubbleSort = new();
+
+var items = bubbleSort.Sort(new int[] { 9, 5, 2, 4, 8, 1 }, Check);
+
+foreach (int item in items)
+{
+    Console.Write(item);
+    Console.Write(' ');
+}
+
+
+
+
+//bool Check(int a, int b)
+//{
+//    return a < b;
+//}
+
+bool Check(int a, int b) => a < b;
+
+Person[] person = new Person[]
+{
+    new Person(){Name="Hasan", Age = 32 },
+    new Person(){Name="Rashid", Age = 30 },
+    new Person(){Name="Hasan", Age = 29 },
+};
+
+BubbleSort<Person> person1 = new();
+var result = person1.Sort(person, ComparePerson);
+
+Console.WriteLine();
+bool ComparePerson(Person a, Person b)
+{
+    if(a.Name == b.Name)
+        return a.Age > b.Age;
+    else
+        return a.Name.CompareTo(b.Name) > 0;
+}
+
+foreach(var item in result)
+{
+    Console.WriteLine($"Name : {item.Name} Age: {item.Age}\n");
+}
+
